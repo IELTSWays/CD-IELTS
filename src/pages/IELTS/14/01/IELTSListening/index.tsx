b9354d33ab96b8ce4a1660b70aab3331e18a2454
mehdinaji <seyedmehdinaji@gmail.com>
2024-01-30 10:47:51 +0330

    update loading page

--- src/pages/IELTS/14/01/IELTSListening/index.tsx
+++ src/pages/IELTS/14/01/IELTSListening/index.tsx
@@ -208,7 +208,7 @@ const index = () => {
         </div>
 
         {isLoading && <div> LOADING... </div>}
-        {1 == 1 &&
+        {isSuccess &&
           <>
             {part === 1 &&
               <>
