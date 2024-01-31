d9fb897e7969e3c993e023d060820d61426b69dd
mehdinaji <seyedmehdinaji@gmail.com>
2024-01-31 14:06:39 +0330

    bugfix audioPlayer

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -1,9 +1,23 @@
 import { useEffect, useRef, useReducer, useCallback, useState } from "react";
 
+// mtu
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Slider from '@mui/material/Slider';
+import VolumeUpIcon from '@mui/icons-material/VolumeUp';
+import VolumeDownIcon from '@mui/icons-material/VolumeDown';
+import VolumeMuteIcon from '@mui/icons-material/VolumeMute';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import PauseIcon from '@mui/icons-material/Pause';
+// mtu
+
 const getTime = (seconds: number) =>
   !seconds ? "00:00" : new Date(seconds * 1000).toISOString().slice(14, 19);
 
 export const AudioPlayer = ({ songs }: any) => {
+
+  const [showVolume, setShowVolume] = useState(false);
+
   const player = useRef<any>(null);
   const [playerState, setPlayerState] = useReducer(
     (state: any, action: any) => ({
@@ -15,7 +29,7 @@ export const AudioPlayer = ({ songs }: any) => {
       isPlaying: false,
       current: 0,
       progress: 0,
-      volume: 100,
+      volume: 50,
       prevVolume: 100
     }
   );
@@ -58,19 +72,19 @@ export const AudioPlayer = ({ songs }: any) => {
     player.current.addEventListener("canplay", () => play(), { once: true });
   };
 
-  const handleSeek = (event) => {
+  const handleSeek = (event: any) => {
     const { value } = event.target;
     setPlayerState({ progress: value });
     player.current.currentTime = (value / 100) * player.current.duration;
   };
 
-  const handleVolumeChange = (event) => {
+  const handleVolumeChange = (event: any) => {
     const newVolume = event.target.value;
     setPlayerState({ volume: newVolume });
     player.current.volume = newVolume / 100;
   };
 
-  const handlePlaylistPlay = (index) => {
+  const handlePlaylistPlay = (index: any) => {
     if (playerState.current === index) {
       pausePlay();
     } else {
@@ -92,7 +106,7 @@ export const AudioPlayer = ({ songs }: any) => {
   useEffect(() => {
     if (!player.current) return;
 
-    const updateProgress = (event) => {
+    const updateProgress = (event: any)  => {
       const progress = Math.floor(
         (event.target.currentTime / event.target.duration) * 100
       );
@@ -109,84 +123,39 @@ export const AudioPlayer = ({ songs }: any) => {
     return () => { };
   }, [player, playNext, setPlayerState]);
 
-  useEffect(() => {
-    localStorage.getItem('counter') && play()
-  }, [localStorage.getItem('counter')])
-
   return (
     <>
-      <div className="player">
+      <>
         <audio
           ref={player}
           src={playerState.songs.at(playerState.current)?.audio}
         />
-        {playerState.songs.at(playerState.current) && (
-          <div className="song">
-            <cite className="title">
-              <a
-                href={playerState.songs.at(playerState.current).attribution}
-                className="link"
-              >
-                "{playerState.songs.at(playerState.current).name}"
-              </a>
-            </cite>
-            <span className="artist">
-              by {playerState.songs.at(playerState.current).artist}
-            </span>
-          </div>
+      </>
+
+      <div className='ielts-footer-btn' onClick={pausePlay}>
+        {playerState.isPlaying ? (
+          <PauseIcon color="action" fontSize="large" />
+        ) : (
+          <PlayArrowIcon color="action" fontSize="large" />
         )}
-
-        <button className="prev" onClick={playPrevious}>
-          <div>PREV</div>
-        </button>
-
-        <button className="play" onClick={pausePlay}>
-          {playerState.isPlaying ? (
-            <div>PAUSE</div>
-          ) : (
-            <div>PLAY</div>
-          )}
-        </button>
-
-        <button className="next" onClick={playNext}>
-          <div>NEXT</div>
-        </button>
-
-        <span className="time">
-          {getTime(player.current?.currentTime)}&nbsp;/&nbsp;
-          {getTime(player.current?.duration)}
-        </span>
-
-        <input
-          type="range"
-          className="progress"
-          min="0"
-          max="100"
-          onChange={handleSeek}
-          value={playerState.progress || 0}
-        />
-
-        <div className="volume">
-          <button onClick={toggleMute}>
-            {playerState.volume === 0 ? (
-              <div>MUTE</div>
-            ) : playerState.volume > 50 ? (
-              <div>V H</div>
-            ) : (
-              <div> VL </div>
-            )}
-          </button>
-          <input
-            type="range"
-            className="volume-slider"
-            min="0"
-            max="100"
-            onChange={handleVolumeChange}
-            value={playerState.volume}
-          />
-        </div>
       </div>
-      <ul className="songs">
+
+      <div className='ielts-footer-btn relative'>
+        <span className="d-flex" onClick={() => setShowVolume(!showVolume)}>
+          {playerState.volume == 0 && <VolumeMuteIcon color="action" fontSize="large" />}
+          {playerState.volume > 0 && playerState.volume < 50 && <VolumeDownIcon color="action" fontSize="large" />}
+          {playerState.volume >= 50 && <VolumeUpIcon color="action" fontSize="large" />}
+        </span>
+        {showVolume &&
+          <Box sx={{ width: 150 }} className="volume-controller">
+            <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+              <Slider aria-label="Volume" value={playerState.volume} defaultValue={50} onChange={handleVolumeChange} />
+            </Stack>
+          </Box>
+        }
+      </div>
+
+      {/* <ul className="songs">
         {playerState.songs.map((song, index) => (
           <li
             key={song.name}
@@ -212,7 +181,7 @@ export const AudioPlayer = ({ songs }: any) => {
             </button>
           </li>
         ))}
-      </ul>
+      </ul> */}
     </>
   );
 };
--- src/layouts/LayoutIELTS.tsx
+++ src/layouts/LayoutIELTS.tsx
@@ -1,17 +1,10 @@
 import { useState, useEffect } from 'react'
-import useSound from 'use-sound'
 import { useLocation, useNavigate } from 'react-router-dom';
 
 // mtu
-import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
-import Slider from '@mui/material/Slider';
 import WifiIcon from '@mui/icons-material/Wifi';
 import ForumIcon from '@mui/icons-material/Forum';
 import VideocamIcon from '@mui/icons-material/Videocam';
-import VolumeUpIcon from '@mui/icons-material/VolumeUp';
-import VolumeDownIcon from '@mui/icons-material/VolumeDown';
-import VolumeMuteIcon from '@mui/icons-material/VolumeMute';
 import EditCalendarIcon from '@mui/icons-material/EditCalendar';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import BatteryChargingFullIcon from '@mui/icons-material/BatteryChargingFull';
@@ -23,52 +16,22 @@ import { useAppSelector } from '@/store/hooks'
 
 import "@/styles/ielts.css"
 import Logo from '@/assets/images/ielts.png'
-import mySound from '@/assets/sounds/14/1/section-1.mp3'
 import ModalOptions from '@/components/IELTS/ModalOptions';
 import useTimeNow from '@/components/useTimeNow';
 import useTimer from '@/components/useTimer';
+
+import { AudioPlayer } from "@/components/AudioPlayer";
+
+import listSongs from '@/pages/IELTS/14/01/IELTSListening/Audio/song.json'
 
 const LayoutIELTS = ({ children }: any) => {
 
   const location = useLocation();
   const navigate = useNavigate();
-  
+
   const writingSaved = useAppSelector((state) => state.user.writingSaved)
   const { timeNow } = useTimeNow();
   const { timer } = useTimer('90')
-
-  // AUDIO
-  const [showVolume, setShowVolume] = useState(false);
-  const [volume, setVolume] = useState<number>(50);
-
-  const handleChange = (_event: Event, newValue: number | number[]) => {
-    setVolume(newValue as number);
-  };
-
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  const [playSound, { pause }] = useSound(mySound,
-    {
-      volume: (volume / 100)
-    },
-    {
-      onplay: () => setIsPlaying(true),
-      onend: () => setIsPlaying(false),
-    })
-
-  const togglePlay = () => {
-    if (isPlaying) {
-      pause();
-    } else {
-      playSound();
-    }
-    setIsPlaying(!isPlaying);
-  }
-
-  useEffect(() => {
-    location.pathname.includes('Listening') && togglePlay()
-  }, []);
-  // AUDIO
 
   return (
     <html data-theme='light' className='ielts'>
@@ -87,11 +50,6 @@ const LayoutIELTS = ({ children }: any) => {
             </div>
 
             <div className='align-items-center g-20'>
-              {location.pathname.includes('Listening') &&
-                <button onClick={() => togglePlay()} style={{ width: '70px' }}>
-                  {isPlaying ? 'pause' : 'Play'}
-                </button>
-              }
               {location.pathname.includes('writing') &&
                 writingSaved === 'true' && 'Saved'
               }
@@ -119,20 +77,9 @@ const LayoutIELTS = ({ children }: any) => {
               <div className='ielts-footer-btn'>
                 <WifiIcon color="action" fontSize="large" />
               </div>
-              <div className='ielts-footer-btn relative'>
-                <span className="d-flex" onClick={() => setShowVolume(!showVolume)}>
-                  {volume == 0 && <VolumeMuteIcon color="action" fontSize="large" />}
-                  {volume > 0 && volume < 50 && <VolumeDownIcon color="action" fontSize="large" />}
-                  {volume >= 50 && <VolumeUpIcon color="action" fontSize="large" />}
-                </span>
-                {showVolume &&
-                  <Box sx={{ width: 150 }} className="volume-controller">
-                    <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
-                      <Slider aria-label="Volume" value={volume} defaultValue={50} onChange={handleChange} />
-                    </Stack>
-                  </Box>
-                }
-              </div>
+              {location.pathname.includes('Listening') &&
+                listSongs.songs.length > 0 && <AudioPlayer songs={listSongs.songs} />
+              }
               <div className='ielts-footer-btn'>
                 Exit
               </div>
