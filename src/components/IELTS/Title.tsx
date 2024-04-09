d6f4c1005bc0c0300317daac4990495bd96e26c7
mehdinaji <seyedmehdinaji@gmail.com>
2024-04-09 21:23:11 +0330

    update header-footer ielts layout

--- src/components/IELTS/Title.tsx
+++ src/components/IELTS/Title.tsx
@@ -1,9 +1,15 @@
+import React from "react";
+
 const Title = ({ title, description }: any) => {
   return (
-    <div className='ielts-container pt-20'>
+    <div className='ielts-container pt-10'>
       <div className='ielts-title'>
-        <div> <strong>{title}</strong> </div>
-        <div> {description} </div>
+        <div>
+          <strong className="pr-10">
+            {title}
+          </strong>
+          {description}
+        </div>
       </div>
     </div>
   );
